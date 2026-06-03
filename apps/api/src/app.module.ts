a25c020c055887bf88825d0b07fe3fc324688977
GandotraDiya <diyagandotra02@gmail.com>
2026-06-03 10:08:28 +0530

    feat(projects): implement project management and membership workflows

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     DatabaseModule,
     AuthModule,
     UsersModule,
+    ProjectsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ apps/api/src/projects/projects.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
+import { ProjectsService } from './projects.service';
+
+import { CreateProjectDto } from './dto/create-project.dto';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+
+import { Param } from '@nestjs/common';
+import { AddProjectMemberDto } from './dto/add-project-member.dto';
+
+@Controller('projects')
+@UseGuards(JwtAuthGuard)
+export class ProjectsController {
+  constructor(
+    private readonly projectsService: ProjectsService,
+  ) {}
+
+  @Post()
+  create(
+    @Body() dto: CreateProjectDto,
+    @CurrentUser() user: any,
+  ) {
+    return this.projectsService.create(
+      dto,
+      user.id,
+      user.organizationId,
+    );
+  }
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Post(':projectId/members')
+addMember(
+  @Param('projectId')
+  projectId: string,
+
+  @Body()
+  dto: AddProjectMemberDto,
+) {
+  return this.projectsService.addMember(
+    projectId,
+    dto.userId,
+  );
+}
+
+@Get(':projectId/members')
+getMembers(
+  @Param('projectId')
+  projectId: string,
+) {
+  return this.projectsService.getMembers(
+    projectId,
+  );
+}
+
+@Get('/my-projects')
+getMyProjects(
+  @CurrentUser() user: any,
+) {
+  return this.projectsService.getMyProjects(
+    user.id,
+  );
+}
+}
